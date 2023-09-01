9904d7f49729ccb788892333c58ff0a4836b8782
Roman Averin <mail@romanaverin.com>
2023-09-01 17:23:41 +0300

    WezTerm init config

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -1,42 +1,59 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
+local config = {}
 
--- This table will hold the configuration.
-local config = {
 
-}
+config.front_end = 'Software'
+
+
+config.audible_bell = "Disabled"
+config.color_scheme = 'Pencil Dark (Gogh)'
+config.font = wezterm.font 'Fira Code'
+config.window_close_confirmation = 'NeverPrompt'
+config.initial_cols = 180
+config.initial_rows = 50
 
 config.keys = {
-{
-	key = 'c',
-    mods = 'CTRL',
-    action = wezterm.action_callback(function(window, pane)
-        selection_text = window:get_selection_text_for_pane(pane)
-        is_selection_active = string.len(selection_text) ~= 0
-        if is_selection_active then
-            window:perform_action(wezterm.action.CopyTo('ClipboardAndPrimarySelection'), pane)
-        else
-            window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane)
-        end
-   	end),
-
-
-},
-    { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
-    { key = 'v', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
+     {
+     key = 'c',
+     mods = 'CTRL',
+     action = wezterm.action_callback(function(window, pane)
+         selection_text = window:get_selection_text_for_pane(pane)
+         is_selection_active = string.len(selection_text) ~= 0
+         if is_selection_active then
+             window:perform_action(wezterm.action.CopyTo('ClipboardAndPrimarySelection'), pane)
+         else
+             window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane)
+         end
+     end),
+     },{
+     key = 'v',
+     mods = 'CTRL',
+     action = wezterm.action_callback(function(window, pane)
+         window:perform_action(act.PasteFrom('Clipboard'), pane)
+         -- window:perform_action(act.PasteFrom('PrimarySelection'), pane)
+     end)
+     },
 }
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
 
--- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
-config.color_scheme = 'Pencil Dark (Gogh)'
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
+}
 
--- and finally, return the configuration to wezterm
+
 return config
+
 
