87fe9f843489879a5c5aa88824a50c18a183fa31
Roman Averin <mail@romanaverin.com>
2024-07-27 10:40:01 +0300

    add wezterm config

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -3,23 +3,59 @@ local act = wezterm.action
 local config = {}
 
 
---local gpus = wezterm.gui.enumerate_gpus()
---config.front_end = 'Software'
 config.animation_fps = 60
-
---config.front_end = 'WebGpu'
---config.webgpu_preferred_adapter = gpus[2]
-config.enable_wayland = true
+config.enable_wayland = false
+config.front_end = "WebGpu"
 
 config.use_fancy_tab_bar = true
+-- config.window_decorations = "INTEGRATED_BUTTONS" 
+config.window_background_opacity = 0.98
 config.audible_bell = "Disabled"
-config.color_scheme = 'Pencil Dark (Gogh)'
---config.color_scheme = 'Relaxed (Gogh)'
+
+-- 
+--  Color scheme
+--
+
+local default_scheme = wezterm.color.get_builtin_schemes()['Sonokai (Gogh)']
+
+-- Override color scheme 
+--default_scheme.background = '#212121'
+--default_scheme.background = '#2c2e34'
+default_scheme.cursor_bg = '#26a269'
+default_scheme.cursor_border = '#26a269'
+--default_scheme.cursor_fg = '#ffffff'
+--default_scheme.selection_fg = 'black'
+--default_scheme.selection_bg = '#79C0FF'
+--default_scheme.split = '#B694DF'
+default_scheme.tab_bar = {
+        inactive_tab_edge = '#575757',
+        active_tab = {
+            bg_color = '#1f1f1f',
+            fg_color = '#c0bfbc',
+        },
+        inactive_tab = {
+            bg_color = '#434750',
+            fg_color = '#808080',
+        },
+}
+
+config.color_schemes = {
+    ['My Pencil Dark'] = default_scheme,
+}
+config.color_scheme = 'My Pencil Dark'
+--
+--
+--
+--
+config.font = wezterm.font { family = 'JetBrainsMono NF', weight = 'ExtraLight' }
+config.font_size = 12.5
 --config.font = wezterm.font { family = 'Fira Code', weight = 'Light' }
-config.font = wezterm.font { family = 'Fira Mono' }
+
 config.window_close_confirmation = 'NeverPrompt'
 config.initial_cols = 180
 config.initial_rows = 50
+
+config.scrollback_lines = 10000
 
 config.inactive_pane_hsb = {
     saturation = 0.9,
@@ -40,7 +76,84 @@ wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
   return zoomed .. index .. tab.active_pane.title
 end)
 
+wezterm.on('update-right-status', function(window, pane)
+    local active_workspace = window:active_workspace()
+    local name = window:active_key_table()
+    if name then
+        name = 'TABLE: ' .. name
+    end
+    local prompt_status = 'Workspace: ' .. active_workspace
+    window:set_right_status(prompt_status)
+end)
+
+
 config.keys = {
+    -- show launcher
+    { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
+
+    -- Switch to the default workspace
+    {
+      key = 'y',
+      mods = 'CTRL|SHIFT',
+      action = act.SwitchToWorkspace {
+        name = 'default',
+      },
+    },
+
+ 
+    -- Switch to btop workspace
+    {
+      key = 'b',
+      mods = 'CTRL|SHIFT',
+      action = act.SwitchToWorkspace {
+      name = 'monitoring',
+      spawn = {
+        args = { 'btop' },
+        },
+      },
+    },
+
+
+    -- Create a new workspace with a random name and switch to it
+    { key = 'i', mods = 'CTRL|SHIFT', action = act.SwitchToWorkspace },
+    -- Show the launcher in fuzzy selection mode and have it list all workspaces
+    -- and allow activating one.
+    {
+      key = '9',
+      mods = 'ALT',
+      action = act.ShowLauncherArgs {
+        flags = 'FUZZY|WORKSPACES',
+      },
+    },
+
+    -- Prompt for a name to use for a new workspace and switch to it.
+    {
+      key = 's',
+      mods = 'CTRL|SHIFT',
+      action = act.PromptInputLine {
+        description = wezterm.format {
+          { Attribute = { Intensity = 'Bold' } },
+          { Foreground = { AnsiColor = 'Fuchsia' } },
+          { Text = 'Enter name for new workspace' },
+        },
+        action = wezterm.action_callback(function(window, pane, line)
+          -- line will be `nil` if they hit escape without entering anything
+          -- An empty string if they just hit enter
+          -- Or the actual line of text they wrote
+          if line then
+            window:perform_action(
+              act.SwitchToWorkspace {
+                name = line,
+              },
+              pane
+            )
+          end
+        end),
+      },
+    },
+
+
+    -- Copy and Paste key mapping
      {
      key = 'c',
      mods = 'CTRL',
@@ -62,33 +175,19 @@ config.keys = {
      end)
      },
 
-     -- Split panel to the right.
-    {
-      key = 'r',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'd',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'r',
-      mods= 'CTRL|SHIFT|SUPER',
-      action = wezterm.action.SplitPane {
-        direction = 'Right',
-        top_level = true
-      }
-    },
-    {
-      key = 'd',
-      mods= 'CTRL|SHIFT|SUPER',
-      action = wezterm.action.SplitPane {
-        direction = 'Down',
-        top_level = true
+     -- Split panel to the right vetrical.
+      {
+        key = '/',
+        mods = 'CTRL|ALT',
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
       },
-    },
+      -- Split panel to the donw horizontal.
+      {
+        key = '-',
+        mods = 'CTRL|ALT',
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+      },
+       
     -- Rotate panes --
     -- See https://wezfurlong.org/wezterm/config/lua/keyassignment/RotatePanes.html --
     {
@@ -105,11 +204,18 @@ config.keys = {
     { key = 'DownArrow', mods = 'LEADER', action = wezterm.action.AdjustPaneSize { 'Down', 5 } },
     { key = 'UpArrow', mods = 'LEADER', action = wezterm.action.AdjustPaneSize { 'Up', 5 } },
     { key = 'RightArrow', mods = 'LEADER', action = wezterm.action.AdjustPaneSize { 'Right', 5 } },
+    
+    -- New pane
+    {
+        key = 't',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.SpawnCommandInNewTab,
+    },
 
-    -- Close pane --
+   -- Close pane --
     {
         key = 'w',
-        mods =  'CTRL',
+        mods =  'CTRL|SHIFT',
         action = wezterm.action.CloseCurrentPane { confirm = true }
     },
 
@@ -122,11 +228,19 @@ config.keys = {
     -- Itentity panel ---
     {
 		key = "I",
-		mods = "CTRL",
+		mods = "CTRL|SHIFT",
 		action = wezterm.action.PaneSelect({
             show_pane_ids = true,
 		}),
 	},
+    -- New Window 
+    {
+        key = "n",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.SpawnCommandInNewWindow {
+            cwd = wezterm.home_dir
+        }
+    },
 }
 
 
@@ -149,7 +263,7 @@ config.mouse_bindings = {
 
 config.window_frame = {
     font_size = 12.0,
-    font = wezterm.font { family = 'Fira Code', weight = 'Medium' },
+    font = wezterm.font { family = 'Fira Code', weight = 'Regular' },
   -- active_titlebar_bg = '#333333',
   -- inactive_titlebar_bg = '#333333',
 }
@@ -160,39 +274,6 @@ config.cursor_blink_rate = 500
 config.cursor_blink_ease_in = 'Constant'
 config.cursor_blink_ease_out = 'Constant'
 
--- Colors --
-config.colors = {
-    cursor_bg = '#52ad70',
-    -- Overrides the text color when the current cell is occupied by the cursor
-    cursor_fg = 'black',
-    -- Specifies the border color of the cursor when the cursor style is set to Block,
-    -- or the color of the vertical or horizontal bar when the cursor style is set to
-    -- Bar or Underline.
-    cursor_border = '#52ad70',
-
-
-   	selection_fg = 'black',
-	selection_bg = '#79C0FF',
-    split = '#B694DF',
-    tab_bar = {
-        -- The color of the inactive tab bar edge/divider
-        inactive_tab_edge = '#575757',
-
-        active_tab = {
-            -- The color of the background area for the tab
-            bg_color = '#1f1f1f',
-            -- The color of the text for the tab
-            fg_color = '#c0bfbc',
-        },
-        inactive_tab = {
-            bg_color = '#434750',
-            fg_color = '#808080',
-        },
-
-
-  },
-
-}
 
 return config
 
