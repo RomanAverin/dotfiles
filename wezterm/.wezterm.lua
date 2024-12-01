470bce07f3201095388da16a818f5ee3ff2b79d5
Roman Averin <mail@romanaverin.com>
2024-12-01 16:28:07 +0300

    Change right status information of the wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -179,13 +179,65 @@ wezterm.on("format-window-title", function(tab, pane, tabs, panes, config)
 end)
 
 wezterm.on("update-right-status", function(window, pane)
+	-- Each element holds the text for a cell in a "powerline" style << fade
+	local cells = {}
+
+	-- Workspace entry
 	local active_workspace = window:active_workspace()
-	local name = window:active_key_table()
-	if name then
-		name = "TABLE: " .. name
+	local table_name = window:active_key_table()
+	-- Active key table
+	if table_name then
+		table_name = "mode: " .. table_name
+		table.insert(cells, table_name or "")
 	end
-	local workspace_prompt = string.format("Workspace: %s %s", active_workspace, name or "")
-	window:set_right_status(workspace_prompt)
+
+	table.insert(cells, active_workspace)
+	-- An entry for each battery (typically 0 or 1 battery)
+	for _, b in ipairs(wezterm.battery_info()) do
+		table.insert(cells, string.format("%.0f%%", b.state_of_charge * 100))
+	end
+
+	-- The powerline < symbol
+	local LEFT_ARROW = utf8.char(0xe0b3)
+	-- The filled in variant of the < symbol
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+	-- Color palette for the backgrounds of each cell
+	local colors = {
+		"#222327",
+		"#2c2e34",
+		"#33353f",
+		"#363944",
+		"#414550",
+	}
+
+	-- Foreground color for the text across the fade
+	local text_fg = "#c0c0c0"
+
+	-- The elements to be formatted
+	local elements = {}
+	-- How many cells have been formatted
+	local num_cells = 0
+
+	-- Translate a cell into elements
+	local function push(text, is_last)
+		local cell_no = num_cells + 1
+		table.insert(elements, { Foreground = { Color = text_fg } })
+		table.insert(elements, { Background = { Color = colors[cell_no] } })
+		table.insert(elements, { Text = " " .. text .. " " })
+		if not is_last then
+			table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
+			table.insert(elements, { Text = SOLID_LEFT_ARROW })
+		end
+		num_cells = num_cells + 1
+	end
+
+	while #cells > 0 do
+		local cell = table.remove(cells, 1)
+		push(cell, #cells == 0)
+	end
+
+	window:set_right_status(wezterm.format(elements))
 end)
 
 -- Key bindings
